af28a34df2b87157bcca3468bedf1131086f0dec
irolram <rrivan@iesrafaelalberti.es>
2024-10-21 19:39:16 +0200

    3,4,5

---
+++ src/ej21_03.py
@@ -0,0 +1,31 @@
+#Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+
+def pedir_numeros():
+
+
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame otro número: "))
+
+
+    return num1, num2
+
+def comprobar_num2(num2):
+
+    while num2 == 0:
+        num2 = int(input("Dame otro número: "))
+    return num2
+    
+def division_de_numeros(num1, num2):
+    
+    division = int(num1 / num2)
+
+    return division
+
+def main():
+    num1, num2 = pedir_numeros()
+    comprobar_num2(num2)
+    division = division_de_numeros(num1, num2)
+    print("La división de los números {}, {} es: {}".format(num1, num2, division))
+
+if __name__ == "__main__":
+    main()
--- src/ej23_01.py
+++ src/ej23_01.py
@@ -1,10 +1,45 @@
-#Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 
-def pedir_numeros():
-    num1 = input("Dame un número: ")
-    num2 = input("Dame un número: ")
+def pedir_edad() ->int:
 
-    return num1, num2
+    edad_incorrecta = True
 
-def division_de_numeros():
+    while edad_incorrecta:
+
+        try:
+            edad  = None
+            edad = int(input("Introduce tu edad: "))
+
+            if edad < 0:
+                raise ValueError("La edad debe ser positiva")
+            if edad == 0:
+                raise ValueError("Tu ere mu chico pa escribir en un ordenador")
+            if edad > 125:
+                raise ValueError("No existe momia tan vieja")
+            
+            edad_incorrecta = False
+        except ValueError as e: 
+            if edad is None:
+
+                print(f"ERROR {e}. Intentalo de nuevo")
+            else:
+                print(f"ERROR {e}. Intentalo de nuevo")
     
+    return edad
+
+def mostrar_anios_cumplidos(edad: int):
+
+    for i in range(1, edad + 1):
+        if i == edad:
+            print(i)
+        else:
+            print(i, end=", ")
+
+def main():
+    edad = pedir_edad()
+    print(f"Has cumplido los siguientes años: ")
+    mostrar_anios_cumplidos(edad)
+
+
+if __name__ == "__main__":
+    main()
