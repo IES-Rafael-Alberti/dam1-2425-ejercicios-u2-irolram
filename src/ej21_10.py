1e8bf06d820936c161eb0e2b86b6868c83d88d74
irolram <rrivan@iesrafaelalberti.es>
2024-10-22 20:40:10 +0200

    arreglar el 10

--- src/ej21_10.py
+++ src/ej21_10.py
@@ -15,9 +15,14 @@ def pizza_vege_ono(pizza):
 
     if pizza == "vegetariana":
         vegetariana = input("Introduzca su ingrediente extra: Pimiento y tofu.  ")
-        print("La pizza vegetariana tiene los ingredientes siguientes:  Mozzarella, tomate y {}".format(vegetariana).lower())
+        while vegetariana != "pimiento" and "tofu":
+            vegetariana = input("Introduzca su ingrediente extra: Pimiento y tofu.  ")
+            print("La pizza vegetariana tiene los ingredientes siguientes:  Mozzarella, tomate y {}".format(vegetariana).lower())
+
     else:
         no_vege = input("Introduzca su ingrediente extra: Peperoni, Jamón y Salmón. ")
+        while no_vege != "jamon" and "peperoni" and "salmon":
+            no_vege = input("Introduzca su ingrediente extra: Peperoni, Jamón y Salmón.  ")
         print("La pizza tiene los ingredientes siguientes: Mozzarella, tomate y {}".format(no_vege).lower())
     
 
