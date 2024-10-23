9587dd1fc8d85aae491a877e2fca5e5d2e391bf4
irolram <rrivan@iesrafaelalberti.es>
2024-10-23 17:30:39 +0200

    1 al 5

---
+++ src/ej22_02.py
@@ -0,0 +1,24 @@
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+
+from ej21_01  import preguntar_edad
+
+
+def todos_anios_cumplido(edad):
+
+    guardar_edad = ""
+
+    for i in range(1, edad + 1):
+        guardar_edad += str(i) + ", "
+        if i == edad:
+            guardar_edad = guardar_edad.removesuffix(", ")
+
+    print("Has cumplido los siguientes años: {}".format(guardar_edad))
+    
+
+
+def main():
+    edad = preguntar_edad()
+    todos_anios_cumplido(edad)
+
+if __name__ == "__main__":
+    main() 
